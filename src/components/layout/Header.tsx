3e43fc43865ecb52e150290e1781fb0d3a754e0d
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2023-11-07 01:43:38 +0530

    Update Readme

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import Heading from "components/atoms/heading/Heading";
-import React, { forwardRef } from "react";
+import  { forwardRef } from "react";
 
 export interface HeaderProps {
   title: string;
